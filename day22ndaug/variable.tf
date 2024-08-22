aaa813f6f3b77e4c59d496f481ffdf4239e895a4
pranitha <siddavatampranitha@gmail.com>
2024-08-22 11:34:29 +0530

    uptodate variables

---
+++ day22ndaug/variable.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+description = "passing the vaules to main.tf"
+type = string
+default = ""
+}
+variable "instance_type" {
+    type = string
+    default = "" 
+}
+variable "key_name" {
+    type = string
+    default = ""
+  
+}
