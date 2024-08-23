d15508fc564ce07cacf2cd5d211ec1a61d91e5fd
pranitha <siddavatampranitha@gmail.com>
2024-08-23 14:07:21 +0530

    yptodate

--- day5/main.tf
+++ day5/main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "name" {
     ami= "ami-0d07675d294f17973"
     instance_type = "t2.micro"
-    key_name = "singapore"
+    key_name = "linux"
     tags = {
-      name ="test"
-    }
+    Name = "testbackend"
+  }
+
  }
  
