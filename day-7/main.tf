45bc76c40deca3d71cfb4859e063ed3d789f1fc5
pranitha <siddavatampranitha@gmail.com>
2024-08-26 20:39:01 +0530

    update

---
+++ day-7/main.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "devvv" {
+    ami= "ami-066784287e358dad1"
+    instance_type = "t2.micro"
+    key_name = "linux"
+ }
+
+
+resource "aws_s3_bucket" "wedd" {
+    bucket = "wedd"
+  
+}
+
+resource "aws_dynamodb_table" "dynamodb-lock" {
+  name = "dynamodb"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+} 
