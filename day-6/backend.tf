1b42cc9ff4282d1a5790f0c561b6ac2c76a96c33
pranitha <siddavatampranitha@gmail.com>
2024-08-29 11:50:26 +0530

    uptodate

--- day-6/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "prani"
-    key    = "singapore/terraform.tfstate"
-    region = "ap-southeast-1"
-    dynamodb_table = "terraform-state-lock-dynamo" 
-       encrypt = "true"
-      }
-}
--- day9/main.tf
+++
@@ -1,7 +0,0 @@
-module "aws" {
-    source = "../day5"
-    ami= "ami-0d07675d294f17973"
-    instance_type = "t2.micro"
-     key_name = "my_key"
-  
-}
