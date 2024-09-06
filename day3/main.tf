056b407d0242c835510cdd77085943eb24e7fabd
pranitha610 <pranithapranitha610@gmail.com>
2024-09-06 13:25:58 +0530

    Update main.tf

--- day3/main.tf
+++ day3/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "name" {
     ami= "ami-04a5ce820a419d6da"
     instance_type = "t2.micro"
-    key_name = "my-key"
+    key_name = "my_key"
     tags = {
     Name = "testbackend"
   }
